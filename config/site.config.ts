82841e38ee7e414bbbe064ec17cab0c7a46f096d
Anuj Sharma <anuj.sharma@travclan.com>
2026-02-02 22:46:15 +0530

    Fix: build issue

--- config/site.config.ts
+++ config/site.config.ts
@@ -60,10 +60,8 @@ export const siteConfig = {
 
 /* ------------------ COMPANY INFO ------------------ */
 export const companyInfo = {
-  mission:
-    "Develop a robust global supply chain to ensure efficient production and distribution for the automotive industry.",
-  vision:
-    "Commitment to innovation, cutting-edge technology, and staying at the forefront of industry advancements.",
-  about:
-    "After a successful launch in 2015, Saroop Industries expanded into a comprehensive automotive solutions provider. With state-of-the-art facilities and world-class manufacturing capabilities, Saroop delivers competitive, cost-effective, and reliable products for the global automotive market.",
+  mission: "Develop a robust global supply chain to ensure efficient production and distribution of automotive industry.",
+  vision: "Our commitment to innovation, cutting-edge technology, and staying at the forefront of industry advancements.",
+  experience: "10+",
+  about: "After a successful launch of the brand in 2015, Saroop is now ready to deliver a wider range of products to the automotive industry. This makes us a one-stop shop for all automotive needs. Saroop has state-of-the-art facilities, world-class capacities, and capabilities that design products using competitive and cost-effective technologies.",
 } as const;
